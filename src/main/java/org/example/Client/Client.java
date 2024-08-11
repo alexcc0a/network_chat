4bfd572c748daaf741a8799ae7f3bc9ae72da220
Alexander Nesterov <alex18nest@icloud.com>
2024-08-11 14:15:38 -0300

    [+] network_chat

---
+++ src/main/java/org/example/Client/Client.java
@@ -0,0 +1,104 @@
+package org.example.Client;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.Properties;
+import java.util.Scanner;
+
+public class Client {
+    private int port;
+    private String host;
+    private final Logger log = Logger.getInstance();
+    private BufferedReader in;
+    private PrintWriter out;
+    private String path;
+    private Socket socket;
+    private Scanner scanner;
+
+    public Client() {
+        Logger log = Logger.getInstance();
+        try (FileReader reader = new FileReader("settings.txt")) {
+            Properties props = new Properties();
+            props.load(reader);
+            port = Integer.parseInt(props.getProperty("SERVER_PORT"));
+            host = props.getProperty("SERVER_HOST");
+            path = props.getProperty("CLIENT_LOG");
+        } catch (IOException ex) {
+            log.log(ex.getMessage(), path);
+            ex.printStackTrace(System.out);
+        }
+
+        try {
+            this.socket = new Socket(host, port);
+        } catch (IOException e) {
+            e.printStackTrace(System.out);
+            log.log(e.getMessage(), path);
+        }
+
+        try {
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true);
+            scanner = new Scanner(System.in);
+
+            String msg;
+            // Узнаем имя, приветствуем
+            System.out.print("Введите Ваше имя: ");
+            msg = scanner.nextLine();
+
+            out.println("/name " + msg);
+            String receivedMsg = "SERVER: " + in.readLine();
+            System.out.println(receivedMsg);
+            log.log(receivedMsg, path);
+
+            new WriteChat().start();
+            new ReadChat().start();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        new Client();
+    }
+
+    //читаем сообщения из сокета
+    private class ReadChat extends Thread {
+        @Override
+        public void run() {
+            String str;
+            try  {
+                while (true) {
+                    System.out.print("> ");
+                    str = in.readLine();
+                    if (str.equals("/exit")) {
+                        log.log(str, path);
+                        break;
+                    }
+                    System.out.println(str);
+                    log.log(str, path);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                log.log(e.getMessage(), path);
+            }
+        }
+    }
+
+    //пишем сообщения в сокет
+    public class WriteChat extends Thread {
+        @Override
+        public void run() {
+            while (true) {
+                String userMsg;
+                userMsg = scanner.nextLine();
+                if (userMsg.equals("/exit"))
+                    break;
+                else {
+                    out.println(userMsg);
+                }
+                out.flush();
+            }
+        }
+    }
+}
---
+++ src/main/java/org/example/Server/Server.java
@@ -0,0 +1,104 @@
+package org.example.Server;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Properties;
+
+public class Server {
+
+    public static ArrayList<Socket> clients = new ArrayList<>();
+
+    public static void main(String[] args) {
+        ServerWork.ConnectWithServer.Connect();
+    }
+
+}
+
+class ServerWork implements Runnable {
+    private static int port;
+    private static String host;
+    private static String path;
+    private static final Logger log = Logger.getInstance();
+    Socket client;
+
+    public ServerWork(Socket socket) {
+        this.client = socket;
+    }
+
+    @Override
+    public void run() {
+        String userName = "";
+
+        log.log("Сервер запущен. Port: " + port + " Host: " + host, path);
+        System.out.println("Сервер запущен. Port: " + port + " Host: " + host);
+
+        while (true) {
+            try {
+                PrintWriter out = new PrintWriter(this.client.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(this.client.getInputStream()));
+
+                String line;
+                while ((line = in.readLine()) != null) {
+                    if (line.lastIndexOf("/name") == 0) {
+                        userName = line;
+                        userName = userName.replace("/name", "").trim();
+                        log.log(userName + " присоединился к чату", path);
+                        send("Привет, " + userName + "!");
+                        continue;
+                    }
+
+                    send('[' + userName + "] " + line);
+                    }
+
+                    if (line.equals("/exit")) {
+                        log.log(userName + " вышел из чата", path);
+                        send(userName + " вышел из чата");
+                        Server.clients.remove(client);
+                        break;
+                    }
+            } catch (IOException ex) {
+                log.log(ex.getMessage(), path);
+                ex.printStackTrace(System.out);
+            }
+        }
+
+    }
+
+    private void send(String message) throws IOException {
+        for (Socket client : Server.clients) {
+            if (client.isClosed()) continue;
+            PrintWriter sender = new PrintWriter(client.getOutputStream());
+            sender.println(message);
+            sender.flush();
+        }
+        log.log(message,path);
+    }
+
+    class ConnectWithServer {
+        public static void Connect() {
+            try (FileReader reader = new FileReader("settings.txt")) {
+                Properties props = new Properties();
+                props.load(reader);
+                port = Integer.parseInt(props.getProperty("SERVER_PORT"));
+                host = props.getProperty("SERVER_HOST");
+                path = props.getProperty("SERVER_LOG");
+            } catch (IOException ex) {
+                log.log(ex.getMessage(), path);
+                System.out.println(ex.getMessage());
+            }
+
+            try (ServerSocket server = new ServerSocket(port)) {
+                while (true) {
+                    Socket client = server.accept();
+                    Server.clients.add(client);
+                    new Thread(new ServerWork(client)).start();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                log.log(e.getMessage(), path);
+            }
+        }
+    }
+}
